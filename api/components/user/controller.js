d468c1ecce8096a43ae28f23ee576f8f4524087a
AlvaroMtz <hola@alvaromtz.es>
2020-01-19 19:51:03 +0100

    storage

---
+++ api/components/user/controller.js
@@ -0,0 +1,11 @@
+const store = require('../../../store/dummy');
+
+const TABLA = 'user';
+
+function list() {
+    return store.list(TABLA);
+}
+
+module.exports = {
+    list
+}
--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const express = require ('express');
 
 const config = require('../config.js');
-const user = require('./components/user/network');
+const user = require('./components/user/network.js');
 
 const app = express();
 
 //ROUTER
-app.use('api/user', user);
+app.use('/api/user', user);
 
 app.listen(config.api.port, () => {
     console.log('API ESCUCHANDO EN EL PUERTO' , config.api.port)
